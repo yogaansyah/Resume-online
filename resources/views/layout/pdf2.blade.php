8862121324400e1b0ebc7d43d14c964d0aff7b43
Yoga <yogahendriansyah@gmail.com>
2022-08-08 18:33:53 +0700

    fix pdf

--- resources/views/layout/pdf2.blade.php
+++ resources/views/layout/pdf2.blade.php
@@ -8,8 +8,8 @@
 
     <link rel="stylesheet" type="text/css"
         {{-- href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
-        integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous" />
-    <link rel="stylesheet" type="text/css" href="https://unpkg.com/aos@2.3.1/dist/aos.css" /> --}}
+        integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous" /> --}}
+    <link rel="stylesheet" type="text/css" href="https://unpkg.com/aos@2.3.1/dist/aos.css" />
     <link rel="stylesheet" type="text/css" href="../../../pdf_yoga/css/common.css" />
     <link rel="stylesheet" type="text/css" href="../../../pdf_yoga/css/fonts.css" />
     <link rel="stylesheet" type="text/css" href="../../../pdf_yoga/css/Page1.css" />
